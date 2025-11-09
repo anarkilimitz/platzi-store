c6265f2de1c8a93725a58ce8734e062c616fc652
Evgenii Borisovich <anarkilimitz@gmail.com>
2025-11-09 17:14:33 +0500

    create-isLoading-error-sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,7 +5,38 @@ import { useSelector } from 'react-redux';
 import styles from './sidebar.module.scss';
 
 const Sidebar = () => {
-	const { list } = useSelector(({ categories }) => categories);
+	const { list, isLoading, error } = useSelector(
+		({ categories }) => categories
+	);
+
+	if (isLoading) {
+		return (
+			<section className={styles.sidebar}>
+				<div className={styles.title}>CATEGORIES</div>
+				<div className={styles.loaderWrapper}>
+					<div className={styles['lds-hourglass']}></div>
+				</div>
+				<div className={styles.footer}>
+					<a href="/help" target="_blank" className={styles.link}>
+						Help
+					</a>
+					<a href="/terms" target="_blank" className={styles.link}>
+						Terms & Conditions
+					</a>
+				</div>
+			</section>
+		);
+	}
+
+	if (error) {
+		return (
+			<section className={styles.sidebar}>
+				<div className={styles.title}>CATEGORIES</div>
+				<div className={styles.error}>Ошибка загрузки</div>
+				<div className={styles.footer}></div>
+			</section>
+		);
+	}
 
 	return (
 		<section className={styles.sidebar}>
@@ -16,7 +47,7 @@ const Sidebar = () => {
 						<li key={id}>
 							<NavLink
 								className={({ isActive }) =>
-									`${styles.link} ${isActive ? styles.active : 'null'}`
+									`${styles.link} ${isActive ? styles.active : ''}`
 								}
 								to={`/categories/${id}`}
 							>
