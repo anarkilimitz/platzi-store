d7e9a9dac5a9520f718c1830ed54afa608f0ef9e
Evgenii Borisovich <anarkilimitz@gmail.com>
2025-11-10 22:00:05 +0500

    product

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -2,10 +2,56 @@ import React from 'react';
 
 import styles from './product.module.scss';
 
-const Product = () => {
-    return <section className={styles.product}>
-        
-    </section>;
+const SIZES = [4, 4.5, 5];
+
+const Product = ({ title, price, images, description }) => {
+	const currentImage = images[0];
+
+	return (
+		<section className={styles.product}>
+			<div className={styles.images}>
+				<div
+					className={styles.current}
+					style={{ backgroundImage: `url(${currentImage})` }}
+				/>
+				{images.map((image, i) => (
+					<div
+						key={i}
+						className={styles.image}
+						style={{ backgroundImage: `url(${image})` }}
+						onClick={() => {}}
+					/>
+				))}
+			</div>
+			<div className={styles.info}>
+				<h1 className={styles.title}>{title}</h1>
+				<div className={styles.price}>{price}</div>
+				<div className={styles.color}>
+					<span>Color:</span>Green
+				</div>
+				<div className={styles.sizes}>
+					<span>Sizes:</span>
+
+					<div className={styles.list}>
+						{SIZES.map((size) => (
+							<div key={size} className={`${styles.size}`} onClick={() => {}}>
+								{size}
+							</div>
+						))}
+					</div>
+				</div>
+				<p className={styles.description}>{description}</p>
+
+				<div className={styles.action}>
+					<button className={styles.add}>Добавить в корзину</button>
+					<button className={styles.favourite}>Добавить в избранное</button>
+				</div>
+				<div className={styles.bottom}>
+					<div className={styles.purchase}>19 people purchased</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Product;
