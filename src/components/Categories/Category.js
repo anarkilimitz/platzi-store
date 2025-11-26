b1d744f6eaf063016a62b648fe7a59b8bb19246a
Evgenii Borisovich <anarkilimitz@gmail.com>
2025-11-26 16:50:15 +0500

    create-category-title-list-cat-offset-limit

--- src/components/Categories/Category.js
+++ src/components/Categories/Category.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react';
 
 import { useParams } from 'react-router-dom';
 import { useGetProductsQuery } from '../../features/api/apiSlice';
+import { useSelector } from 'react-redux';
+
 import Products from '../../components/Products/Products';
 
 import styles from './category.module.scss';
 
 const Category = () => {
 	const { id } = useParams();
+	// беру категории из стора, чтобы отображались в заголовке h2 (cat)
+	const { list } = useSelector(({ categories }) => categories);
 
 	const defaultValues = {
 		title: '',
@@ -16,11 +20,17 @@ const Category = () => {
 	};
 	const defaultParams = {
 		categoryId: id,
+		limit: 8,
+		offset: 0,
 		...defaultValues,
 	};
 
+	const [cat, setCat] = useState('');
+	// const [items, setItems] = useState([]);
 	const [values, setValues] = useState(defaultValues);
 	const [params, setParams] = useState(defaultParams);
+
+	const { data = [], isLoading, isSuccess } = useGetProductsQuery(params);
 
 	useEffect(() => {
 		if (!id) return;
@@ -28,7 +38,25 @@ const Category = () => {
 		setParams({ ...defaultParams, categoryId: id });
 	}, [id]);
 
-	const { data, isLoading, isSuccess } = useGetProductsQuery(params);
+	// useEffect(() => {
+	// 	if (!isLoading) return;
+
+	// 	const products = Object.values(data);
+
+	// 	if (!products.length) return;
+
+	// 	setItems((_items) => [..._items, ...products]);
+	// }, [data, isLoading]);
+
+	useEffect(() => {
+		if (!id || !list.length) {
+			setCat('');
+			return;
+		}
+
+		const category = list.find((item) => item.id === Number(id));
+		setCat(category?.name || 'Категория не найдена');
+	}, [list, id]);
 
 	const handleChange = ({ target: { value, name } }) => {
 		setValues({ ...values, [name]: value });
@@ -42,7 +70,7 @@ const Category = () => {
 
 	return (
 		<section className={styles.wrapper}>
-			<h2 className={styles.title}>Товары</h2>
+			<h2 className={styles.title}>{cat}</h2>
 			<form className={styles.filters} onSubmit={handleSubmit}>
 				<div className={styles.filter}>
 					<label>Название товара</label>
